bd3bd954872d1792547e5a17da90030a77f9d0a9
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2022-12-16 14:42:49 +0100

    Task 02 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+  gallery.insertAdjacentHTML(
+    "afterbegin",
+    `<a class="gallery__item" href=${item.original}>
+  <img class="gallery__image" src=${item.preview} alt="${item.description}"/>
+</a>`
+  );
+});
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
